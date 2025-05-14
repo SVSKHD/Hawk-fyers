086551cd2c10542055fd66cd493816bf3e3a4c4d
HITHESH SUNKARA <hithesh_sunkara@epam.com>
2025-05-14 21:55:43 +0530

    env fix

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import os
 from dotenv import load_dotenv
+from fyers_apiv3 import fyersModel
 
+load_dotenv()
 
+load_dotenv()
+
+api = os.getenv("api")
+secret_id = os.getenv("secret_id")
+app_id = os.getenv("app_id")
+redirect_url = os.getenv("redirect_url")
+
+# connection = fyersModel.FyersModel(client_id=api, secret_key=secret_id, redirect_uri=redirect_url, response_type="code", grant_type="authorization")
